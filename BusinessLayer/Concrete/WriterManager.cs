1cc6ab2bed03dac47ae4adc6aecf43cd6922e6d1
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-10-14 02:24:22 +0300

    34.DERS

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+  public class WriterManager : IWriterService
+    {
+        IWriterDal _writerDal;
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void Add(Writer writer)
+        {
+            _writerDal.Add(writer);
+        }
+    }
+}
---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+       WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer writer, string passwordAgain)
+        {
+            if (  writer.Password == passwordAgain)
+            {
+
+                writer.Status = true;
+                wm.Add(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+
+            else
+            {
+                ModelState.AddModelError("Password", "Girdiğiniz Parola Eşleşmedi, Lütfen Tekrar Deneyin");
+            }
+            return View();
+
+        }
+    }
+}
