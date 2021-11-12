b39f0aeea4f02adb986b4ae260f780b83a91425d
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-11-12 18:03:24 +0300

    62.ders

---
+++ CoreDemo/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogsListWithCategory();
+            return View(values);
+        }
+    }
+}
