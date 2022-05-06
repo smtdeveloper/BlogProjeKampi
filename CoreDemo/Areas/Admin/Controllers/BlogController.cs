7f3e03a3b455524497bfa875786c84ed7d55b419
Samet Akca <developer2@cevizsoft.com>
2022-05-06 03:42:32 +0300

    95 - 96. Excel Export Static

---
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,65 @@
+﻿using ClosedXML.Excel;
+using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "BlogID"; 
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+
+                int BlogRowCount = 2;
+
+                foreach (var item in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+
+                using(var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var context = stream.ToArray();
+                    return File(context, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Dosya.xlsx");
+                }
+
+            }
+            
+
+              
+        }
+
+
+        public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+        public List<BlogModel> GetBlogList()
+        {
+
+            var bm = new List<BlogModel>
+            {
+                new BlogModel{ID = 1 , BlogName= "Samet Akca"},
+                new BlogModel{ID = 2 , BlogName= "Eslem Betül"},
+                new BlogModel{ID = 3 , BlogName= "test"}
+            };
+            return bm;
+
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/Models/BlogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Models
+{
+    public class BlogModel
+    {
+        public int ID { get; set; }
+        public string BlogName { get; set; }
+    }
+}
