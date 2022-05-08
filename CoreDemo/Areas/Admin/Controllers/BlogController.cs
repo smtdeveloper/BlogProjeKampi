3f5750f82c04d1045810786bb37b5b9302a1764e
Samet Akca <developer2@cevizsoft.com>
2022-05-08 15:03:11 +0300

    97. DinamikExcelBlogList

--- CoreDemo/Areas/Admin/Controllers/BlogController.cs
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using CoreDemo.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -44,6 +45,37 @@ namespace CoreDemo.Areas.Admin.Controllers
               
         }
 
+        public IActionResult ExportDinamikExcelBlogList()
+        {
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "BlogID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+
+                int BlogRowCount = 2;
+
+                foreach (var item in GetDinamikBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var context = stream.ToArray();
+                    return File(context, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Dosya.xlsx");
+                }
+
+            }
+
+
+
+        }
 
         public IActionResult BlogListExcel()
         {
@@ -60,6 +92,27 @@ namespace CoreDemo.Areas.Admin.Controllers
             };
             return bm;
 
+        }
+
+        public List<BlogModel> GetDinamikBlogList()
+        {
+            var bm = new List<BlogModel>();
+
+            using (var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel
+                {
+                    ID = x.BlogId,
+                    BlogName = x.Title
+                }).ToList();
+            }
+
+            return bm;
+        }
+
+        public IActionResult DinamikBlogList()
+        {
+            return View();
         }
     }
 }
