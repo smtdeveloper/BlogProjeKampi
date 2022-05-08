7e7693a48f9083c12b8aad388b0e235f33353d24
Samet Akca <developer2@cevizsoft.com>
2022-05-08 17:21:40 +0300

    98 - Widget 99- istatistik 1

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context(); 
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount = bm.TGetAll().Count();
+            ViewBag.MessageCount = c.Contacts.Count();
+            ViewBag.CommentCount = c.Comments.Count();
+           
+            return View();
+        }
+    }
+}
