af7cbbba7f66eeec657af074f754d5dc5614b22a
Samet Akca <developer2@cevizsoft.com>
2022-05-09 07:14:07 +0300

    100. istatistik 2

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.LastBlogTitle = c.Blogs.OrderByDescending(x => x.BlogId).Select(x => x.Title).Take(1).FirstOrDefault();
+
+            return View();
+        }
+    }
+}
