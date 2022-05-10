cff656fb5df18842049ba2b489e34fe45aa6f941
Samet Akca <developer2@cevizsoft.com>
2022-05-10 18:03:20 +0300

    103.apiden hava durumunu cekme

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,7 +19,14 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.BlogCount = bm.TGetAll().Count();
             ViewBag.MessageCount = c.Contacts.Count();
             ViewBag.CommentCount = c.Comments.Count();
-           
+
+            string api = "59ae7e420ee51ebffa30a7642e93774b";
+            string city = "kutahya";
+            string connection =
+                "https://api.openweathermap.org/data/2.5/weather?q=kütahya&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.HavaDurumu = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
