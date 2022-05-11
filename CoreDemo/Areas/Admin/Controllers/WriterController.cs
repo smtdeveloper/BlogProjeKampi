1058dba3137465327232c2775ebec2dd6f60531e
Samet Akca <developer2@cevizsoft.com>
2022-05-11 18:47:08 +0300

    106-107 Ajax ile listeleme

---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,52 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id = 1,
+                Name = "Samet"
+            },
+            new WriterClass
+            {
+                Id = 2,
+                Name = "Şevval"
+            },
+            new WriterClass
+            {
+                Id = 3,
+                Name = "Nurana"
+            },
+            new WriterClass
+            {
+                Id = 4,
+                Name = "Mete Serkan"
+            }
+
+        };
+
+    }
+}
---
+++ CoreDemo/Areas/Admin/Models/WriterClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Models
+{
+    public class WriterClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
