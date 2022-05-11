8a6026560103bc8096bd5900397c24ac61fdf620
Samet Akca <developer2@cevizsoft.com>
2022-05-11 09:43:49 +0300

    104- 105 chart  yapısını  static

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,43 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass
+            {
+                categoryname = "Teknoloji",
+                categorycount = 10
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Yazılım",
+                categorycount = 14
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Spor",
+                categorycount = 5
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Sinema",
+                categorycount = 2
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/Models/CategoryClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Models
+{
+    public class CategoryClass
+    {
+        public int categorycount { get; set; }
+        public string categoryname { get; set; }
+    }
+}
