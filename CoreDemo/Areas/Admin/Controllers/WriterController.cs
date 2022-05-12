76bd9e8081a19332235e08b00f09686699c200e8
Samet Akca <developer2@cevizsoft.com>
2022-05-12 18:03:11 +0300

    Ajax işlemleri bitti

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -23,6 +23,37 @@ namespace CoreDemo.Areas.Admin.Controllers
             return Json(jsonWriters);
         }
 
+        public IActionResult GetWriterByID(int writerid)
+        {
+          var findWriter =  writers.FirstOrDefault(x => x.Id == writerid);
+          var JsonWriters = JsonConvert.SerializeObject(findWriter);
+          return Json(JsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+           var JsonWriters = JsonConvert.SerializeObject(w);
+            return Json(JsonWriters);
+        }
+
+      
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(w);
+            return Json(writer);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
