6d216ce9072651de090421e982690e70110437a6
Samet Akca <developer2@cevizsoft.com>
2022-05-15 00:52:25 +0300

    api silme update işlemleri

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,82 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+         public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var value = c.Employees.ToList();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult EmployeeAdd(Employee employee)
+        {
+            using var c = new Context();
+            c.Add(employee);
+            c.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EmployeeGet(int id)
+        {
+            using var c = new Context();
+            var value = c.Employees.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new Context();
+            var value = c.Employees.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Remove(value);
+                c.SaveChanges();
+                return Ok(value);
+            }
+           
+        }
+
+        [HttpPut]
+        public IActionResult EmployeeUpdate(Employee employee)
+        {
+            using var c = new Context();
+            var value = c.Find<Employee>(employee.ID);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                value.Name = employee.Name;
+                c.Update(value);
+                c.SaveChanges();
+                return Ok();
+
+            }
+        }
+
+    }
+}
