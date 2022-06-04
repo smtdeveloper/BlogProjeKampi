63c5479e6671740b2a82ac6267992e037b8af8c3
Samet Akca <developer2@cevizsoft.com>
2022-06-04 17:04:53 +0300

    Admin Blog listelenmesi

---
+++ CoreDemo/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+         
+        public IActionResult Index()
+        {
+            var values = blogManager.GetBlogsListWithCategory();
+            return View(values);
+        }
+
+
+    }
+}
